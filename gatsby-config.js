3bdff06fba6822ce458729279a0d1a288cc215a0
adrenalize-brad <brad@adrenalizedigital.ca>
2021-10-28 14:56:46 -0600

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `neural Smart Business Technologies`,
-    description: `Custom smart business applications. neuralSBT - It's All Connected.`,
-    keywords: 'Web, App, Application, Software, Development, Design',
+    title: `Jax Bucerias - Home Of The Best Live Music In The Bay!`,
+    description: `Jax`,
+    keywords: 'Jax',
   },
   plugins: [
     {
@@ -14,31 +14,23 @@ module.exports = {
         token: process.env.GATSBY_GRAPHCMS_TOKEN,
       },
     },
-    //{
-     // resolve: `gatsby-plugin-manifest`,
-      //options: {
-       // name: 'Meet Brad - Freelance Dev Extraordinaire',
-       // short_name: `Brad S. CV`,
-       // background_color: `#ffffff`,
-       // lang: `en`,
-       // theme_color: `#ffffff`,
-       // start_url: `/`,
-       // display: `standalone`,
-       // cache_busting_mode: 'none',
-       // icon: `src/assets/images/icon.png`,
-       // include_favicon: true,
-       // icon_options: {
-        //  purpose: `any maskable`,
-        //}
-      //}
-    //},
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
+        name: 'Jax Bucerias - Home Of The Best Live Music In The Bay!',
+        short_name: `Jax Bucerias`,
+        background_color: `#ffffff`,
+        lang: `en`,
+        theme_color: `#ffffff`,
+        start_url: `/`,
+        display: `standalone`,
+        cache_busting_mode: 'none',
+        icon: `src/assets/images/circle-logo-no-bg.png`,
+        include_favicon: true,
+        icon_options: {
+          purpose: `any maskable`,
+        }
+      }
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-offline`,
